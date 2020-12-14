80153656753e7326039804459f3aa9bb0928d4ea
Ilya Nemolyaev <megataiger@gmail.com>
2020-12-14 19:23:15 +0400

    Add delStuff method

--- src/main/java/DataBase.java
+++ src/main/java/DataBase.java
@@ -15,6 +15,7 @@ public class DataBase {
             preparedStatement.setDouble(2, stuffPrice);
 
             preparedStatement.executeUpdate();
+            System.out.println("Запись добавлена");
         }
     }
 
@@ -39,6 +40,19 @@ public class DataBase {
                 System.out.print(resultSet.getInt(1) + " ");
                 System.out.print(resultSet.getString(2) + " ");
                 System.out.println(resultSet.getDouble(3) + " ");
+            }
+        }
+    }
+
+    public static void delStuff(Connection connection, int id) throws SQLException {
+        System.out.println("Начинаю процедуру удаления");
+        try (PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM shop WHERE shop.id = ?")) {
+            preparedStatement.setInt(1, id);
+
+            if (preparedStatement.executeUpdate() == 0) {
+                System.out.println("Записей с таким id не найдено");
+            } else {
+                System.out.println("Удаление строки прошло успешно");
             }
         }
     }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,15 +5,15 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws SQLException {
         try (Connection connection = DriverManager.getConnection("jdbc:h2:~/test", "ilya", "")) {
-            DataBase.addStuff(connection);
-            DataBase.addStuff(connection);
-            DataBase.addStuff(connection);
 
             DataBase.getStuff(connection);
 
             System.out.println();
 
-            DataBase.getStuffById(connection,1);
+            DataBase.delStuff(connection,6);
+
+            DataBase.getStuff(connection);
+
         }
     }
 }
